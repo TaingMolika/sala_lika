6d80892186ed1f3231adcba131ea9dab2793dd25
Molika Taing <taingmolika@gmail.com>
2022-07-29 15:29:44 +0700

    First Commit

---
+++ pages/product.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container mx-auto mt-8">
+        <ProductCard 
+        v-for="mountain in mountains" 
+        :key="mountain.title" 
+        :product="mountains" 
+        class="mb-4"
+        />
+    </div>
+</template>
+<script>
+export default {
+    async fetch() {
+        this.mountains = await fetch(
+            'https://api.nuxtjs.dev/mountains'
+        ).then(res => res.json())
+    },
+    data() {
+        return {
+            mountains: []
+        }
+    }
+}
+</script>
+<style lang="postcss">
+img{
+    height: 120px;
+}
+</style>
